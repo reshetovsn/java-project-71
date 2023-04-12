1f856837616bb38c723bd9caeee1e251beeea23e
Sergey Reshetov <reshetov.sn@gmail.com>
2023-04-12 13:22:51 +0300

    Add Parser.java

---
+++ app/src/main/java/hexlet.code/Parser.java
@@ -0,0 +1,35 @@
+package hexlet.code;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Parser {
+    public static String parsingRes(Map<String, Objects> mapA, Map<String, Objects> mapB) {
+        Set<String> keys = new TreeSet<>();
+        keys.addAll(mapA.keySet());
+        keys.addAll(mapB.keySet());
+
+        StringBuilder result = new StringBuilder();
+        result.append("{\n");
+        for (String elements : keys) {
+            var valueA = mapA.get(elements);
+            var valueB = mapB.get(elements);
+            if (mapA.containsKey(elements) && !mapB.containsKey(elements)) {
+                result.append("- ").append(elements).append(": ").append(valueA).append("\n");
+            } else if (!mapA.containsKey(elements) && mapB.containsKey(elements)) {
+                result.append("+ ").append(elements).append(": ").append(valueB).append("\n");
+            } else if (mapA.containsKey(elements) && mapB.containsKey(elements)) {
+                if (valueA.equals(valueB)) {
+                    result.append("  ").append(elements).append(": ").append(valueA).append("\n");
+                } else {
+                    result.append("- ").append(elements).append(": ").append(valueA).append("\n");
+                    result.append("+ ").append(elements).append(": ").append(valueB).append("\n");
+                }
+            }
+        }
+        result.append("}");
+        return result.toString();
+    }
+}
