09f46fd714ed0a7d26edb838fb9045009ed10eff
Sergey Reshetov <reshetov.sn@gmail.com>
2023-04-13 10:59:41 +0300

    Fixed linter mistakes DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,12 +1,9 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-
 //import java.nio.file.Files;
 //import java.nio.file.Path;
 //import java.nio.file.Paths;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DifferTest {
