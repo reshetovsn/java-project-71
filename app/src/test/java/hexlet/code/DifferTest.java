672b639edda576302635ff88966cf8d28cb54760
Sergey Reshetov <reshetov.sn@gmail.com>
2023-04-14 11:38:26 +0300

    Modified DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -11,17 +11,34 @@ class DifferTest {
     void testJSON() throws Exception {
         String filepath1 = "src/test/resources/file1.json";
         String filepath2 = "src/test/resources/file2.json";
-        String actual = Differ.generate(filepath1, filepath2);
+        String actual = Differ.generate(filepath1, filepath2, "stylish");
 //        Path result = Paths.get("src/test/resources/result_json.txt").toAbsolutePath().normalize();
 //        String expected = Files.readString(result);
         String expected = """
                 {
-                - follow: false
-                  host: hexlet.io
-                - proxy: 123.234.53.22
-                - timeout: 50
-                + timeout: 20
-                + verbose: true
+                  chars1: [a, b, c]
+                - chars2: [d, e, f]
+                + chars2: false
+                - checked: false
+                + checked: true
+                - default: null
+                + default: [value1, value2]
+                - id: 45
+                + id: null
+                - key1: value1
+                + key2: value2
+                  numbers1: [1, 2, 3, 4]
+                - numbers2: [2, 3, 4, 5]
+                + numbers2: [22, 33, 44, 55]
+                - numbers3: [3, 4, 5]
+                + numbers4: [4, 5, 6]
+                + obj1: {nestedKey=value, isNested=true}
+                - setting1: Some value
+                + setting1: Another value
+                - setting2: 200
+                + setting2: 300
+                - setting3: true
+                + setting3: none
                 }""";
         assertThat(actual).isEqualTo(expected);
     }
@@ -30,17 +47,34 @@ class DifferTest {
     void testYAML() throws Exception {
         String filepath1 = "src/test/resources/file1.yaml";
         String filepath2 = "src/test/resources/file2.yaml";
-        String actual = Differ.generate(filepath1, filepath2);
+        String actual = Differ.generate(filepath1, filepath2, "stylish");
 //        Path result = Paths.get("src/test/resources/result_json.txt").toAbsolutePath().normalize();
 //        String expected = Files.readString(result);
         String expected = """
                 {
-                - follow: false
-                  host: hexlet.io
-                - proxy: 123.234.53.22
-                - timeout: 50
-                + timeout: 20
-                + verbose: true
+                  chars1: [a, b, c]
+                - chars2: [d, e, f]
+                + chars2: false
+                - checked: false
+                + checked: true
+                - default: null
+                + default: [value1, value2]
+                - id: 45
+                + id: null
+                - key1: value1
+                + key2: value2
+                  numbers1: [1, 2, 3, 4]
+                - numbers2: [2, 3, 4, 5]
+                + numbers2: [22, 33, 44, 55]
+                - numbers3: [3, 4, 5]
+                + numbers4: [4, 5, 6]
+                + obj1: {nestedKey=value, isNested=true}
+                - setting1: Some value
+                + setting1: Another value
+                - setting2: 200
+                + setting2: 300
+                - setting3: true
+                + setting3: none
                 }""";
         assertThat(actual).isEqualTo(expected);
     }
