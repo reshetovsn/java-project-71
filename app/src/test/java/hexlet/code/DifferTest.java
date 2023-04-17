de8a8992b7e2403895d90b376afb8a2b9d5af60c
Sergey Reshetov <reshetov.sn@gmail.com>
2023-04-17 10:50:47 +0300

    Modified DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -113,7 +113,7 @@ class DifferTest {
     }
 
     @Test
-    void StylishAsDefault() throws Exception {
+    void stylishAsDefault() throws Exception {
         String filepath1 = "src/test/resources/file1.json";
         String filepath2 = "src/test/resources/file2.json";
         String format = "stylish";
