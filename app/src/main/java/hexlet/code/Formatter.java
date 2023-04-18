f9e76f431a4d7c7805068844506c1e8151d0c6e4
Sergey Reshetov <reshetov.sn@gmail.com>
2023-04-18 22:40:38 +0300

    Modified Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -3,14 +3,12 @@ package hexlet.code;
 import hexlet.code.Formatters.Json;
 import hexlet.code.Formatters.Plain;
 import hexlet.code.Formatters.Stylish;
-
 import java.util.List;
 import java.util.Map;
 
 public class Formatter {
-    public static String chooseFormat(Map<String, Object> mapA, Map<String, Object> mapB, String format)
+    public static String chooseFormat(List<Map<String, Object>> difference, String format)
             throws Exception {
-        List<Map<String, Object>> difference = Difference.makeDifference(mapA, mapB);
         return switch (format) {
             case "stylish" -> Stylish.makeStylish(difference);
             case "plain" -> Plain.makePlain(difference);
