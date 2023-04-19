f497698382a3d636593e9cfcfcad596e1a01e223
Sergey Reshetov <reshetov.sn@gmail.com>
2023-04-19 16:44:05 +0300

    Modified Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -6,13 +6,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> makeParsing(String filepath, String content) throws Exception {
+    public static Map<String, Object> makeParsing(String stringToDir, String content) throws Exception {
         Map<String, Object> map = new HashMap<>();
         ObjectMapper mapper;
-        if (filepath.contains(".json")) {
+        if (stringToDir.contains(".json")) {
             mapper = new ObjectMapper();
             map = mapper.readValue(content, Map.class);
-        } else if (filepath.contains(".yaml") || filepath.contains(".yml")) {
+        } else if (stringToDir.contains(".yaml") || stringToDir.contains(".yml")) {
             mapper = new ObjectMapper(new YAMLFactory());
             map = mapper.readValue(content, Map.class);
         }
